9f0829802f0acee8cc87903e53d9e6f6c6257432
Mayank Bansal <mayankbansal125@gmail.com>
2024-11-10 20:43:50 +0530

    feat: add content.js, logic for blocking and redirecting

--- popup.js
+++ popup.js
@@ -1,24 +1,41 @@
-
 let blockedUrlsDiv=document.getElementById("blocked-urls")
+let messageDiv=document.getElementById("message")
+let inputDiv=document.getElementById("urlInput")
 
-document.getElementById('addButton').addEventListener('click', () => {
-    const url = document.getElementById('urlInput').value;
+document.getElementById("addButton").addEventListener("click", () => {
+  const url = inputDiv.value.trim().replace(/^https?:\/\//, "").replace(/^www\./, "");  
     if (url) {
       chrome.storage.sync.get({ blockedUrls: [] }, (data) => {
-        const blockedUrls = data.blockedUrls;
-        blockedUrls.push(url);
-        chrome.storage.sync.set({ blockedUrls });
+        const updatedBlockedSites=[...data.blockedUrls, url]
+        chrome.storage.sync.set({blockedUrls: updatedBlockedSites},()=>{
+          messageDiv.textContent="Site is added to blocked list!"
+          setTimeout(()=>{
+            messageDiv.textContent=""
+          },3000)
+          inputDiv.value=""
+        })
       });
     }
 });
 
-chrome.storage.sync.get({ blockedUrls: [] }, (data) => {
+
+function displayBlockedSites(){
+  chrome.storage.sync.get({ blockedUrls: [] }, (data) => {
     const blockedUrls = data.blockedUrls;
-    if(blockedUrls && blockedUrls.length > 0){
-        blockedUrls.forEach(url => {
-            let newList=document.createElement("li");
-            newList.textContent=url;
-            blockedUrlsDiv.appendChild(newList);
-        });
+    let newLine=document.createElement("p");
+    if(!blockedUrls || blockedUrls.length===0){
+      newLine.textContent="No Blocked Sites to show!"
+      blockedUrlsDiv.appendChild(newLine)
+      return;
     }
-});
+    newLine.textContent="Here are the sites that are blocked:"
+    blockedUrlsDiv.appendChild(newLine)
+    blockedUrls.forEach(url => {
+        let newList=document.createElement("li");
+        newList.textContent=url;
+        blockedUrlsDiv.appendChild(newList);
+    });
+  });
+}
+
+displayBlockedSites()
