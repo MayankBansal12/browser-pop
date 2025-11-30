5355c1e4ff3a000016b409e790d56f2a6e69472e
Mayank Bansal <mayankbansal125@gmail.com>
2025-12-01 03:18:20 +0530

    feat: add support for diff browsers and add polyfill fallback

--- content.js
+++ content.js
@@ -1,59 +1,69 @@
+if (typeof browser === "undefined") {
+  var browser = chrome;
+}
+
 async function checkExtensionStatus() {
-    const data = await chrome.storage.sync.get({ isActive: true });
-    return data.isActive;
+  const result = await browser.storage.sync.get("isActive");
+  const isActive =
+    result && typeof result.isActive !== "undefined" ? result.isActive : true;
+  return isActive;
 }
 
 async function handleBlockedUrls() {
-    const isActive = await checkExtensionStatus();
-    if (!isActive) {
-        return;
-    }
+  const isActive = await checkExtensionStatus();
+  if (!isActive) {
+    return;
+  }
 
-    const { blockedUrls } = await chrome.storage.sync.get({ blockedUrls: [] });
-    if (!blockedUrls.length) {
-        return;
-    }
+  const result = await browser.storage.sync.get("blockedUrls");
+  const blockedUrls =
+    result && Array.isArray(result.blockedUrls) ? result.blockedUrls : [];
+  if (!blockedUrls.length) {
+    return;
+  }
 
-    const isWithinActiveRange = await checkActiveRange()
-    if (!isWithinActiveRange) {
-        return;
-    }
+  const isWithinActiveRange = await checkActiveRange();
+  if (!isWithinActiveRange) {
+    return;
+  }
 
-    const currentUrl = window.location.href;
-    const currentDomain = window.location.hostname.replace("www.", "");
-    blockedUrls.forEach(entry => {
-        if (entry.url === currentDomain && entry.open === true) {
-            const blockedUrl = chrome.runtime.getURL(`blocked.html?url=${decodeURIComponent(currentUrl)}`);
-            window.location.href = blockedUrl;
-        }
-    });
+  const currentUrl = window.location.href;
+  const currentDomain = window.location.hostname.replace("www.", "");
+  blockedUrls.forEach((entry) => {
+    if (entry.url === currentDomain && entry.open === true) {
+      const blockedUrl = browser.runtime.getURL(
+        `blocked.html?url=${decodeURIComponent(currentUrl)}`,
+      );
+      window.location.href = blockedUrl;
+    }
+  });
 }
 
 async function checkActiveRange() {
-    const data = await chrome.storage.sync.get({ startTime: null, endTime: null })
-    const startTime = data.startTime
-    const endTime = data.endTime
-    if (!startTime || !endTime || startTime === "--:--" || endTime === "--:--") {
-        return true;
-    }
-    const date = new Date()
-    const currentTime = date.getHours() + ":" + date.getMinutes()
+  const result = await browser.storage.sync.get(["startTime", "endTime"]);
+  const startTime = result && result.startTime ? result.startTime : null;
+  const endTime = result && result.endTime ? result.endTime : null;
+  if (!startTime || !endTime || startTime === "--:--" || endTime === "--:--") {
+    return true;
+  }
+  const date = new Date();
+  const currentTime = date.getHours() + ":" + date.getMinutes();
 
-    const now = parseTime(currentTime);
-    let start = parseTime(startTime);
-    let end = parseTime(endTime);
-    if (end < start) {
-        start.setDate(start.getDate() - 1);
-    }
-    const isWithinRange = now >= start && now <= end;
-    return isWithinRange;
+  const now = parseTime(currentTime);
+  let start = parseTime(startTime);
+  let end = parseTime(endTime);
+  if (end < start) {
+    start.setDate(start.getDate() - 1);
+  }
+  const isWithinRange = now >= start && now <= end;
+  return isWithinRange;
 }
 
 function parseTime(time, baseDate = new Date()) {
-    const [hours, minutes] = time.split(":").map(Number);
-    const parsed = new Date(baseDate);
-    parsed.setHours(hours, minutes, 0, 0);
-    return parsed;
+  const [hours, minutes] = time.split(":").map(Number);
+  const parsed = new Date(baseDate);
+  parsed.setHours(hours, minutes, 0, 0);
+  return parsed;
 }
 
 handleBlockedUrls();
